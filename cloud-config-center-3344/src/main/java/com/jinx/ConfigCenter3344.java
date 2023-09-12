65b243a54a1ca68b2329b31314e1204ddf2af76e
jinmouX <99781985+jinmouX@users.noreply.github.com>
2023-09-12 19:15:32 +0800

    first commit

---
+++ cloud-config-center-3344/src/main/java/com/jinx/ConfigCenter3344.java
@@ -0,0 +1,19 @@
+package com.jinx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenter3344
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigCenter3344.class,args);
+    }
+}
